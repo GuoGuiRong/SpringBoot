3e2cb9ee33a532e43fd2a33ae866e2c8b5b71983
GuoGuiRong <3095764372@qq.com>
2017-05-18 18:45:48 +0800

    *引入springboot的solr模块,搭建solr全文检索服务模块

---
+++ ggsolr/src/main/java/com/ggr/GgsolrApplication.java
@@ -0,0 +1,12 @@
+package com.ggr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgsolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgsolrApplication.class, args);
+	}
+}
